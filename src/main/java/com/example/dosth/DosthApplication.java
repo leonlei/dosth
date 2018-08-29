8c4a4640aee06dd9004f5216ff2eb114c8de58d0
leiy <leiy@dxy.cn>
2018-08-29 20:31:37 +0800

    first commit

---
+++ src/main/java/com/example/dosth/DosthApplication.java
@@ -0,0 +1,12 @@
+package com.example.dosth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosthApplication.class, args);
+	}
+}
